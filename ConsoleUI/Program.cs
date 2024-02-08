621502e31df318ba4f8a0c26d48b3d2bc9b9bc50
Pierre Plourde <pierre@spartancsharp.net>
2024-02-08 10:31:23 -0500

    Code Cleanup
    Clear Errors, Warnings, and Messages; run Code Cleanup Profile

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main()
 		{
 			List<PersonModel> people = new List<PersonModel>();
 			bool isnotdone = true;
